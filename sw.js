737e937ef110eeeec4e73f743ba8a4a57419256c
Brett Stevenson <therealbrettstevenson@gmail.com>
2019-06-28 20:11:56 -0700

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c3e0346f9ef197a63f83.js"
+    "url": "webpack-runtime-2d0b8f07770042487c5b.js"
   },
   {
     "url": "app-64e044e7235b3cd0bc8f.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b1bf69e5e855a5dd959e7f93606a8f49"
+    "revision": "af1692e7dba0bd160424780e1b66a74b"
   },
   {
     "url": "static/d/604/path---offline-plugin-app-shell-fallback-a-30-c5a-BawJvyh36KKFwbrWPg4a4aYuc8.json"
