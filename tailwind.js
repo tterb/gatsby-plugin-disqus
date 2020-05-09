3173652c3ff75973ad762d3b0c156b3f2d5b3a3b
Brett Stevenson <bstevensondev@gmail.com>
2020-05-08 23:00:19 -0700

    Update tailwind config

--- tailwind.js
+++ tailwind.js
@@ -282,7 +282,7 @@ module.exports = {
     xl: '1.25rem', // 20px
     '2xl': '1.5rem', // 24px
     '3xl': '1.875rem', // 30px
-    '4xl': '2.25rem', // 36px
+    '4xl': '2.75rem', // 36px
     '5xl': '3.75rem',
     '6xl': '4.25rem',
     '7xl': '5.5rem',
